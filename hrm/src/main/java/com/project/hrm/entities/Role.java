37780a4b088f9f1ab848640cc9984840729ead57
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-06-18 21:13:32 +0700

    feat(entity): add relationship between Role and Department entities

--- hrm/src/main/java/com/project/hrm/entities/Role.java
+++ hrm/src/main/java/com/project/hrm/entities/Role.java
@@ -1,7 +1,10 @@
 package com.project.hrm.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +19,8 @@ public class Role {
     @Id
     private Integer id;
     private String name;
+
+    @ManyToOne
+    @JoinColumn
+    private Departments departments;
 }
