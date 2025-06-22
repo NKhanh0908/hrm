f06b3597c917f7dd7ed0e06d11705fdbd3eb22b9
TDKhoa2712 <khoatd2712@gmail.com>
2025-06-23 03:35:14 +0700

    chore(module): clean up old module payroll

--- hrm/src/main/java/com/project/hrm/repositories/DeductionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.hrm.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.hrm.entities.Deduction;
-
-public interface DeductionRepository extends JpaRepository<Deduction, Integer>{
-}
--- hrm/src/main/java/com/project/hrm/repositories/SalaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.hrm.repositories;
-
-import com.project.hrm.entities.Salary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalaryRepository extends JpaRepository<Salary, Integer> {
-}
