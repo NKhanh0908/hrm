c0d6f13761a8a31874b33a2fdc81a2bb70ffc9a3
TDKhoa2712 <khoatd2712@gmail.com>
2025-06-23 03:37:08 +0700

    chore(repository): subsidy repository of old module

--- hrm/src/main/java/com/project/hrm/repositories/SubsidyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.hrm.repositories;
-
-import com.project.hrm.entities.Subsidy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubsidyRepository extends JpaRepository<Subsidy, Integer> {
-}
