03b6761b87e12ba41a4c6138527c6d368dd7d76f
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-06-22 22:00:54 +0700

    efactor(entity): new entity AssignedWorkPerson  and FeedbackEmployee

---
+++ hrm/src/main/java/com/project/hrm/entities/AssignedWorkPerson.java
@@ -0,0 +1,43 @@
+package com.project.hrm.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AssignedWorkPerson {
+    @Id
+    private Integer id;
+    private String title;
+    private String description;
+    private String measurableOutcome;
+    private LocalDateTime startDate;
+    private LocalDateTime targetDate;
+    private LocalDateTime completedDate;
+
+    private Integer progressPercentage;
+    private String progressNotes;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Employees employee;
+
+    @ManyToOne
+    @JoinColumn
+    private Employees assignedBy;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
