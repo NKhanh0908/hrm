7149d3727a73d4af2dbfd8efaa521a8d9d11dbc6
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-06-24 11:18:58 +0700

    feat(mapper): add mapper AssignedWorkPerson

---
+++ hrm/src/main/java/com/project/hrm/dto/assignedWorkPersonDTO/AssignedWorkPersonFilter.java
@@ -0,0 +1,10 @@
+package com.project.hrm.dto.assignedWorkPersonDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class AssignedWorkPersonFilter {
+}
---
+++ hrm/src/main/java/com/project/hrm/mapper/AssignedWorkPersonMapper.java
@@ -0,0 +1,48 @@
+package com.project.hrm.mapper;
+
+import com.project.hrm.dto.assignedWorkPersonDTO.AssignedWorkPersonCreateDTO;
+import com.project.hrm.dto.assignedWorkPersonDTO.AssignedWorkPersonDTO;
+import com.project.hrm.entities.AssignedWorkPerson;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AssignedWorkPersonMapper {
+    public AssignedWorkPerson convertCreateDTOToEntity(AssignedWorkPersonCreateDTO assignedWorkPersonCreateDTO){
+        return AssignedWorkPerson.builder()
+                .title(assignedWorkPersonCreateDTO.getTitle())
+                .description(assignedWorkPersonCreateDTO.getDescription())
+                .startDate(assignedWorkPersonCreateDTO.getStartDate())
+                .targetDate(assignedWorkPersonCreateDTO.getTargetDate())
+                .progressPercentage(0)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public AssignedWorkPersonDTO convertEntityToDTO(AssignedWorkPerson assignedWorkPerson){
+        return AssignedWorkPersonDTO.builder()
+                .id(assignedWorkPerson.getId())
+                .title(assignedWorkPerson.getTitle())
+                .description(assignedWorkPerson.getDescription())
+                .startDate(assignedWorkPerson.getStartDate())
+                .targetDate(assignedWorkPerson.getTargetDate())
+                .progressPercentage(assignedWorkPerson.getProgressPercentage())
+                .progressNotes(assignedWorkPerson.getProgressNotes()!= null ? assignedWorkPerson.getProgressNotes(): null)
+                .employeeId(assignedWorkPerson.getEmployee().getId())
+                .employeeName(assignedWorkPerson.getEmployee().fullName())
+                .assignedById(assignedWorkPerson.getAssignedBy().getId())
+                .assignedByName(assignedWorkPerson.getAssignedBy().fullName())
+                .createAt(assignedWorkPerson.getCreatedAt())
+                .updateAt(assignedWorkPerson.getUpdatedAt() != null ? assignedWorkPerson.getUpdatedAt() : null)
+                .build();
+    }
+
+    public List<AssignedWorkPersonDTO> convertEntityListToDTOList(List<AssignedWorkPerson> assignedWorkPersonList){
+        return assignedWorkPersonList.stream()
+                .map(this::convertEntityToDTO)
+                .collect(Collectors.toList());
+    }
+}
