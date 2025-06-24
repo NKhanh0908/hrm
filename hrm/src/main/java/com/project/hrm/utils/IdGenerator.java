a9105407df84d2ca1353994f43ef415bd26aa3cd
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-06-24 17:27:01 +0700

    feat(utils): add generateNameFile

--- hrm/src/main/java/com/project/hrm/utils/IdGenerator.java
+++ hrm/src/main/java/com/project/hrm/utils/IdGenerator.java
@@ -1,11 +1,25 @@
 package com.project.hrm.utils;
 
+import java.security.SecureRandom;
 import java.util.UUID;
 
 public class IdGenerator {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789".toUpperCase();
+    private static final int ID_LENGTH = 10;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
     public static Integer getGenerationId(){
         UUID uuid = UUID.randomUUID();
 
         return (int) (uuid.getMostSignificantBits() & 0xFFFFFFFFL);
+    }
+
+    public static String generateNameFile() {
+        StringBuilder id = new StringBuilder(ID_LENGTH);
+        for (int i = 0; i < ID_LENGTH; i++) {
+            int index = RANDOM.nextInt(CHARACTERS.length());
+            id.append(CHARACTERS.charAt(index));
+        }
+        return id.toString();
     }
 }
