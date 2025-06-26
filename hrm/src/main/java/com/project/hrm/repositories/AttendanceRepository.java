f6da7a46213e142acbb37da26338b2247545df0a
TDKhoa2712 <khoatd2712@gmail.com>
2025-06-26 21:24:46 +0700

    feature(repository): add new query, remove method

--- hrm/src/main/java/com/project/hrm/repositories/AttendanceRepository.java
+++ hrm/src/main/java/com/project/hrm/repositories/AttendanceRepository.java
@@ -3,8 +3,20 @@ package com.project.hrm.repositories;
 import com.project.hrm.entities.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer>, JpaSpecificationExecutor<Attendance> {
+    @Query("SELECT COUNT(a) > 0 FROM Attendance a WHERE " +
+            "a.checkIn >= :startOfDay AND a.checkIn <= :endOfDay AND a.checkOut IS NULL")
+    boolean existsCheckInOnDateWithoutCheckOut(@Param("startOfDay") LocalDateTime startOfDay,
+                                               @Param("endOfDay") LocalDateTime endOfDay);
+
+    Optional<Attendance> findFirstByEmployee_IdAndCheckOutIsNull(Integer employeeId);
+
 }
--- hrm/src/main/java/com/project/hrm/repositories/SystemRegulationRepository.java
+++ hrm/src/main/java/com/project/hrm/repositories/SystemRegulationRepository.java
@@ -5,9 +5,6 @@ import com.project.hrm.entities.SystemRegulation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface SystemRegulationRepository extends JpaRepository<SystemRegulation, SystemRegulationKey> {
-    Optional<SystemRegulation> findByKey(SystemRegulationKey key);
 }
