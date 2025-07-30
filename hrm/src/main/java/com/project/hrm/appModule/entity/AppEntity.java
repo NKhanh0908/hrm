cb5518d98c88406861c6dff3c9ad43916b4f885b
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-07-30 20:45:41 +0700

    refator: Edit key redis

---
+++ hrm/src/main/java/com/project/hrm/appModule/entity/AppEntity.java
@@ -0,0 +1,4 @@
+package com.project.hrm.appModule.entity;
+
+public class AppEntity {
+}
--- hrm/src/main/java/com/project/hrm/common/redis/RedisKeys.java
+++ hrm/src/main/java/com/project/hrm/common/redis/RedisKeys.java
@@ -3,24 +3,28 @@ package com.project.hrm.common.redis;
 public class RedisKeys {
     private RedisKeys() {}
 
+    //{app}:{module}:{entity}:{identifier}:{attribute}
+
+    public static final String REDIS_PREFIX = "hrm:";
+
     // Training Program Keys
-    public static final String TRAINING_PROGRAM_PREFIX = "training:program:";
-    public static final String TRAINING_PROGRAMS_LIST = "training:programs:list";
+    public static final String TRAINING_PROGRAM_PREFIX = REDIS_PREFIX + "training:program:";
+    public static final String TRAINING_PROGRAMS_LIST = REDIS_PREFIX + "training:programs:list:";
 
     // Training Session Keys
-    public static final String TRAINING_SESSIONS_LIST = "training:sessions:list";
+    public static final String TRAINING_SESSIONS_LIST = REDIS_PREFIX + "training:sessions:list:";
 
     // Recruitment Keys
-    public static final String RECRUITMENT_PREFIX = "recruitment:";
-    public static final String RECRUITMENT_LIST = "recruitment:list:";
+    public static final String RECRUITMENT_PREFIX = REDIS_PREFIX + "recruitment:";
+    public static final String RECRUITMENT_LIST = REDIS_PREFIX + "recruitment:list:";
 
     // Employee Keys
-    public static final String EMPLOYEE_PREFIX = "employee:";
-    public static final String EMPLOYEES_LIST = "employees:list";
+    public static final String EMPLOYEE_PREFIX = REDIS_PREFIX + "employee:";
+    public static final String EMPLOYEES_LIST = REDIS_PREFIX + "employees:list:";
 
     // OTP Keys
-    public static final String OTP_PREFIX = "otp:";
-    public static final String OTP_ATTEMPT_PREFIX = "otp_attempt:";
+    public static final String OTP_PREFIX = REDIS_PREFIX + "auth:otp:";
+    public static final String OTP_ATTEMPT_PREFIX = REDIS_PREFIX + "auth:otp-attempt:";
 
     public static String trainingProgramKey(Integer programId) {
         return TRAINING_PROGRAM_PREFIX + programId;
